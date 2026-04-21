a39dde7c7485804b9f126ea9d830ce4b45302b29
Omkar Pashupatimath <omkar.cs24@bmsce.ac.in>
2026-04-21 10:15:02 +0530

    Update kruskals.c

--- kruskals.c
+++ kruskals.c
@@ -1,48 +1,53 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #define MAX 20
 #define INF 9999
 
 int parent[MAX];
 
-// Find function (Disjoint Set)
+// Find root of a node
 int find(int i) {
-    while (parent[i])
+    while (parent[i] != -1)
         i = parent[i];
     return i;
 }
 
-// Union function
-void union_set(int i, int j) {
-    parent[j] = i;
+// Union of two sets
+void union_set(int u, int v) {
+    parent[v] = u;
 }
 
 int main() {
-    int n, i, j, a, b, u, v;
-    int min, mincost = 0;
+    int n, i, j;
     int cost[MAX][MAX];
+    int min, mincost = 0;
+    int a, b, u, v;
+
+    // Initialize parent array
+    for (i = 0; i < MAX; i++)
+        parent[i] = -1;
 
     printf("Enter number of vertices: ");
     scanf("%d", &n);
 
     printf("Enter adjacency matrix (0 for no edge):\n");
-    for (i = 1; i <= n; i++) {
-        for (j = 1; j <= n; j++) {
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
             scanf("%d", &cost[i][j]);
             if (cost[i][j] == 0)
-                cost[i][j] = INF; // No edge treated as infinity
+                cost[i][j] = INF;
         }
     }
 
     printf("\nEdges in Minimum Spanning Tree:\n");
 
-    for (int edge_count = 1; edge_count < n; edge_count++) {
+    // Kruskal's algorithm
+    for (int edge_count = 0; edge_count < n - 1; edge_count++) {
         min = INF;
 
-        // Find smallest edge
-        for (i = 1; i <= n; i++) {
-            for (j = 1; j <= n; j++) {
+        // Find minimum edge
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
                 if (cost[i][j] < min) {
                     min = cost[i][j];
                     a = u = i;
@@ -51,18 +56,19 @@ int main() {
             }
         }
 
-        // Find roots of vertices
+        // Find roots
         u = find(u);
         v = find(v);
 
         // If no cycle, include edge
         if (u != v) {
-            printf("Edge %d: (%d -> %d) cost = %d\n", edge_count, a, b, min);
+            printf("Edge %d: (%d -> %d) cost = %d\n",
+                   edge_count + 1, a, b, min);
             mincost += min;
             union_set(u, v);
         }
 
-        // Remove edge from consideration
+        // Remove edge from matrix
         cost[a][b] = cost[b][a] = INF;
     }
 
